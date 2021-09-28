0e2271f503584fc204bdbf47a0d90e91f155d3de
Makalee Beelek <makalee.richardson@aggiemail.usu.edu>
2021-09-28 17:08:59 -0600

    Finished the uniform distribution function. No Errors or Warnings.

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <random>
+
 class DistributionPair
 {
   public:
@@ -13,31 +16,79 @@ class DistributionPair
     std::uint32_t count;
 };
 
-#include <iostream>
-
 std::vector<DistributionPair> generateUniformDistribution(std::uint32_t howMany, std::uint32_t min, std::uint32_t max, std::uint8_t numberBins)
 {
+    std::uint32_t step = howMany / numberBins;
+    std::vector<DistributionPair> uniformDist;
 
-}
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::uniform_int_distribution<int> dist(min, max);
 
-std::vector<DistributionPair> generateNormalDistribution(std::uint32_t howMany, float mean, float stdev, std::uint8_t numberBins)
-{
+    for (std::uint8_t i = 0; i < numberBins; i++)
+    {
+        if (i == 0)
+        {
+            uniformDist.push_back(DistributionPair(0, step));
+        }
+        else
+        {
+            auto previousIndex = i - 1;
+            std::uint32_t previousMax = uniformDist[previousIndex].maxValue;
+            uniformDist.push_back(DistributionPair(previousMax + 1, previousMax + 1 + step));
+        }
+    }
 
-}
+    for (std::uint32_t j = 0; j < howMany; j++)
+    {
+        std::uint32_t randNum = dist(engine);
 
-std::vector<DistributionPair> generatePoissonDistribution(std::uint32_t howMany, std::uint8_t howOften, std::uint8_t numberBins)
-{
+        for (int k = 0; k < numberBins; k++)
+        {
+            if (k == 0)
+            {
+                if (randNum <= uniformDist[k].maxValue)
+                {
+                    uniformDist[k].count++;
+                }
+            }
+            else if (k == numberBins - 1)
+            {
+                if (randNum >= uniformDist[k].minValue)
+                {
+                    uniformDist[k].count++;
+                }
+            }
+            else
+            {
+                if (randNum >= uniformDist[k].minValue && randNum <= uniformDist[k].maxValue)
+                {
+                    uniformDist[k].count++;
+                }
+            }
+        }
+    }
+    return uniformDist;
+};
 
-}
+// std::vector < DistributionPair> generateNormalDistribution(std::uint32_t howMany, float mean, float stdev, std::uint8_t numberBins)
+//{
+//};
 
-void plotDistribution(std::string title, const std::vector<DistributionPair>& distribution, const std::uint8_t maxPlotLineSize)
-{
+//std::vector<DistributionPair> generatePoissonDistribution(std::uint32_t howMany, std::uint8_t howOften, std::uint8_t numberBins)
+//{
+//};
 
-}
+//void plotDistribution(std::string title, const std::vector<DistributionPair>& distribution, const std::uint8_t maxPlotLineSize)
+//{
+//};
 
 int main()
+{
+    std::vector<DistributionPair> uniformDist = generateUniformDistribution(10, 1, 10, 2);
+    for (int i = 0; i < uniformDist.size(); i++)
     {
-    std::cout << "Hello World!" << std::endl;
+        std::cout << uniformDist[i].count << std::endl;
+    }
     return 0;
 }
-
